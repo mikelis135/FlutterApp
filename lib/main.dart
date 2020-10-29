ecb277246bd3d1a856fb65e55222529f23cffe21
taiwoadebayo <mikelis135@gmail.com>
2020-10-29 21:07:29 +0100

    Flutter Sample app

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AppHomeWidget(),
+    );
+  }
+}
+
+class AppHomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text("TestApp")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              title: Text("Home", style: TextStyle(color: Colors.lightBlue)),
+              icon: Icon(Icons.home, color: Colors.lightBlue)),
+          BottomNavigationBarItem(
+              title: Text("Cards", style: TextStyle(color: Colors.lightBlue)),
+              icon: Icon(Icons.credit_card, color: Colors.lightBlue)),
+          BottomNavigationBarItem(
+              title: Text("Transactions",
+                  style: TextStyle(color: Colors.lightBlue)),
+              icon: Icon(Icons.history, color: Colors.lightBlue)),
+          BottomNavigationBarItem(
+              title:
+                  Text("Settings", style: TextStyle(color: Colors.lightBlue)),
+              icon: Icon(Icons.settings, color: Colors.lightBlue))
+        ],
+      ),
+      body: AppBodyWidget(),
+    );
+  }
+}
+
+class AppBodyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
